b3b8571b1ec2691ecea3f7e043405e31047d4c5b
subsir <tiancaizhangdaxian@sjtu.edu.cn>
2024-05-19 14:12:41 +0800

    maxlen = 400,400

--- src/file_storage.hpp
+++ src/file_storage.hpp
@@ -1986,7 +1986,7 @@ private:
     buffer *pre = nullptr, *next = nullptr;
   } *head = nullptr, *tail = nullptr;
   int length = 0;
-  const int Maxlength = 600;
+  const int Maxlength = 400;
   map<int, buffer *> buffer_map;
 
 public:
